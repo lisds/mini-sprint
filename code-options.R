c26541c93c335521745ff4a410cc8773c514760b
adamrkenny <adam.kenny@lis.ac.uk>
2025-02-20 13:06:51 +0000

    Updated options.

---
+++ code-options.R
@@ -0,0 +1,22 @@
+##### 
+
+## try adding these, what happens?
+  theme_classic()
+
+  ylim(0, 100) +
+  labs(title = "What does it show?")  
+  xlab("helping behaviour") +
+  ylab("percentage (%)") +
+
+  scale_fill_brewer("area", palette = "Pastel1") +
+  
+
+  coord_flip()
+
+## load packages
+library("ggthemes")
+  
+  scale_fill_economist() +
+  theme_economist
+  
+  
